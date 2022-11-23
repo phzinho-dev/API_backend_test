24f3ce0eac17db22dfbb6e594b6e3ecc56528b3c
phelippe vasquez <phelippevasquez@gmail.com>
2022-11-23 02:02:35 -0300

    config heroku

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/api", require('./router/router'));
 
-app.listen(8080,() => {
-    console.log('servidor funcionando');
-});
+app.listen(process.env.PORT || 3000)
