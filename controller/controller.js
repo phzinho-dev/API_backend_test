db9b427cb2377cb35a3123c00ac47b8802edc89e
phelippe vasquez <phelippevasquez@gmail.com>
2022-11-23 02:30:51 -0300

    testes url

--- controller/controller.js
+++ controller/controller.js
@@ -4,16 +4,16 @@ async function searchUsersGitHub(req, res) {
     if(req.params.name !== undefined){
         const { name } = req.params;
         let request = await axios.get(`https://api.github.com/users/${name}`);
-        return JSON.stringify(res.json({
+        return res.json({
             nome: request.data.name,
             id: request.data.id,
             typeUser: request.data.type,
             avatar: request.data.avatar_url
-        }));
+        });
     }
 
     let request = await axios.get('https://api.github.com/users');
-    return JSON.stringify(res.json(data))
+    return res.json(request.data);
 }
 
 
@@ -21,7 +21,7 @@ async function searchUseGitHubDetails(req, res) {
     const { name } = req.params;
 
     let {data} = await axios.get(`https://api.github.com/users/${name}`)
-    return JSON.stringify(res.json(data))
+    return res.json(data)
 }
 
 
@@ -29,7 +29,7 @@ async function searchUseGitHubRep(req, res) {
     const { name } = req.params;
 
     let {data} = await axios.get(`https://api.github.com/users/${name}/repos`)
-    return JSON.stringify(res.json(data))
+    return res.json(data)
 }
 
 
